459145a65d7a3da9e4a08d2012354833823c4d1c
Вадим <vadim624790@mail.ru>
2021-05-22 11:36:47 +0500

    v1.3

--- api_yamdb/settings.py
+++ api_yamdb/settings.py
@@ -7,7 +7,7 @@ load_dotenv('.env')
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv('SECRET_KEY') or 'dfdfdfdf'
+SECRET_KEY = os.getenv('SECRET_KEY') or 'example'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
